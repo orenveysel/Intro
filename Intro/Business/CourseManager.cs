fb14dc9733232817c5c48b4e03ed7555f9b76776
Veysel ÖREN <orenveysel@outlook.com>
2024-02-23 21:29:15 +0300

    Add project files.

---
+++ Intro/Business/CourseManager.cs
@@ -0,0 +1,21 @@
+﻿using Intro.DataAccess.Concretes;
+using Intro.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro.Business;
+
+public class CourseManager
+{
+
+    public List<Course> GetAll()
+    {
+        //business rules
+        CourseDal courseDal = new CourseDal();
+        //veri kaynağından çekilir...
+        return courseDal.GetAll();
+    }
+}
